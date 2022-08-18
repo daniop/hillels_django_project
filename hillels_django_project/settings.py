c08a52aae3ede5c49e1e8e5106c22c0def87535e
Danylo Tseselskyi <danylotseselskyi@gmail.com>
2022-08-18 09:53:14 +0300

    redis_cashes

--- hillels_django_project/settings.py
+++ hillels_django_project/settings.py
@@ -129,3 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+    }
+}
